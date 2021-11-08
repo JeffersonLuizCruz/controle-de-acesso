1c882cf212cbcfb65b100d921085f4586cd65d83
Jefferson Luiz <jefferson.luiz.cruz@gmail.com>
2021-11-07 23:44:57 -0300

    Adiciona repository RequestStageMapper

---
+++ src/main/java/com/overlay/repository/mapper/RequestStageMapper.java
@@ -0,0 +1,19 @@
+package com.overlay.repository.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.overlay.model.RequestStage;
+import com.overlay.model.User;
+
+@Mapper
+public interface RequestStageMapper {
+	
+	RequestStage getById(Long id);
+	List<User> listAll();
+	RequestStage save(RequestStage user);
+	RequestStage update(RequestStage user);
+	void delete(Long id);
+
+}
