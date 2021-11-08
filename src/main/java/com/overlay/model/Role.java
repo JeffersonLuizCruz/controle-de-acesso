58b2669d9e24c4ff9caa45b2992a8944b0a812f7
Jefferson Luiz <jefferson.luiz.cruz@gmail.com>
2021-11-08 00:54:01 -0300

    Adiciona definição de enum

--- src/main/java/com/overlay/model/Role.java
+++ src/main/java/com/overlay/model/Role.java
@@ -1,10 +1,37 @@
 package com.overlay.model;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 public enum Role {
-	
-	ADMINISTRATOR,
-	SIMPLE;
-	
-	
+
+	ADMINISTRATOR(1), SIMPLE(2);
+
+	int id;
+
+	public static Optional<Role> find(String s) {
+		Predicate<Role> predicate = p -> p.name().equals(s);
+		return Arrays.asList(values()).stream().filter(predicate).findAny();
+	}
+
+	public static Optional<Role> get(int value) {
+		if (value == ADMINISTRATOR.getId()) {
+			return Optional.of(ADMINISTRATOR);
+		}
+
+		if (value == SIMPLE.getId()) {
+			return Optional.of(SIMPLE);
+		}
+		return Optional.empty();
+	}
+
+	private Role(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
 
 }
